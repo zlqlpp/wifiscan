c1092fc6e736d2f67baab59e396f2310a6d3aa7c
zlq <zlqlpp@gmail.com>
2023-08-05 16:35:45 +0800

    1

--- src/main/java/com/cpst/wifiscan/controller/MainController/MainController.java
+++ src/main/java/com/cpst/wifiscan/controller/MainController/MainController.java
@@ -4,6 +4,7 @@ import com.cpst.wifiscan.domain.WifiName;
 import com.cpst.wifiscan.service.WifiService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import java.util.List;
 @Api(tags = "wifi接口")
 @RestController
 @RequestMapping("/wifiscan")
+@Slf4j
 public class MainController {
 
     @Autowired
@@ -23,7 +25,7 @@ public class MainController {
     //@RequestMapping("/findAll")
     @GetMapping("/findAll")
     public List<WifiName> findAll() {
-
+        log.info("findAll");
         return wifiService.findWifiName();
     }
 }
